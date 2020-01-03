664635947b68ea296f3ecaaa06d64b8d4c22b1c8
Conrad Taylor <conradwt@gmail.com>
2020-01-03 12:27:26 -0800

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Conrad Taylor on 1/3/20.
+//  Copyright © 2020 Conrad Taylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
